4f480b1098a8ad230b2b2cfb89e24983545b2aa8
vipin sharma <vipin.sharma502@gmail.com>
2024-03-25 13:03:17 +0530

    Chapter1: taco cloud simple controller with "/" mapping target to "home" view.

---
+++ src/main/java/com/example/springinaction6edtacocloud/SpringInAction6EdTacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinaction6edtacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInAction6EdTacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInAction6EdTacoCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinaction6edtacocloud/SpringInAction6EdTacoCloudApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.springinaction6edtacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class SpringInAction6EdTacoCloudApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+
+}
